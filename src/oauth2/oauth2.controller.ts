31ea6868a0b6b0d702fb67752d1c6c6ffafc5174
lenconda <i@lenconda.top>
2022-04-27 23:20:00 +0800

    feat: 🎸 oauth2 access token validation api

--- src/oauth2/oauth2.controller.ts
+++ src/oauth2/oauth2.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Post,
+} from '@nestjs/common';
+import { Oauth2Service } from './oauth2.service';
 
-@Controller('oauth2')
-export class Oauth2Controller {}
+@Controller('/oauth2')
+export class Oauth2Controller {
+    public constructor(
+        private readonly oauth2Service: Oauth2Service,
+    ) {}
+
+    @Post('/validate')
+    public async validateOauth2AccessToken(
+        @Body('token') token: string,
+        @Body('audience') audience: string | string[],
+    ) {
+        return await this.oauth2Service.validateOauth2AccessToken(token, audience);
+    }
+}
